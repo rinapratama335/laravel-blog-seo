ec9161b74a01cc4c8b41ece91e8426b0b46b06fe
Irwanto Wibowo <42676690+rinapratama335@users.noreply.github.com>
2020-05-07 04:32:49 +0700

    mengintegarasikan template backend (#1)

---
+++ resources/views/template_backend/home.blade.php
@@ -0,0 +1,24 @@
+      <!-- header -->
+      @include('template_backend.header')
+
+      <!-- Sidebar Menu -->
+      @include('template_backend.sidebar')
+
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          <div class="section-header">
+            <h1>Blank Page</h1>
+          </div>
+
+          @yield('content')
+
+          <div class="section-body">
+          </div>
+        </section>
+      </div>
+
+      <!-- Footer -->
+      @include('template_backend.footer')
+
+
