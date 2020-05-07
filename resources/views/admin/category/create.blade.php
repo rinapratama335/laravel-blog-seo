d5d7d1bee337206ac39be06b5b6af086447ba980
Irwanto Wibowo <rinapratama335@gmail.com>
2020-05-07 16:54:11 +0700

    membuat tambah data kategori dan slug kategori

---
+++ resources/views/admin/category/create.blade.php
@@ -0,0 +1,16 @@
+@extends('template_backend.home')
+
+@section('subjudul', 'Tambah Kategori')
+
+@section('content')
+    <form method="post" action="{{ route('category.store') }}">
+        @csrf {{-- Ini wajib ada di form laravel, karena untuk keamanan --}}
+        <div class="form-group">
+            <label>Nama Kategori</label>
+            <input type="text" class="form-control" name="name">
+        </div>
+        <div class="form-group">
+            <button class="btn btn-primary btn-sm btn-block">Simpan</button>
+        </div>
+    </form>
+@endsection
