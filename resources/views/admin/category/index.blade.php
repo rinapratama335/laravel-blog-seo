07856fdabe2b191b68115ad8df56a0e7c23b5b3c
Irwanto Wibowo <42676690+rinapratama335@users.noreply.github.com>
2020-05-07 11:42:22 +0700

    membuat pagination dan membuat nomor item berurut (#3)

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,8 +1,8 @@
 @extends('template_backend.home')
 
 @section('content')
-<h2>Ini adalah Category</h2>
-@foreach($category as $hasil)
+    <h2>Ini adalah Category</h2>
+
     <table class="table table-striped table-hover table-bordered table-sm">
         <thead>
             <tr>
@@ -12,15 +12,17 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>{{ $hasil->id }}</td>
-                <td>{{ $hasil->name }}</td>
-                <td>
-                    <a href="" class="btn btn-warning btn-sm">Edit</a>
-                    <a href="" class="btn btn-danger btn-sm">Hapus</a>
-                </td>
-            </tr>
+            @foreach($category as $hasil => $data)
+                <tr>
+                    <td>{{ $hasil + $category->firstitem() }}</td>
+                    <td>{{ $data->name }}</td>
+                    <td>
+                        <a href="" class="btn btn-warning btn-sm">Edit</a>
+                        <a href="" class="btn btn-danger btn-sm">Hapus</a>
+                    </td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
-@endforeach
+    {{ $category->links() }}
 @endsection
