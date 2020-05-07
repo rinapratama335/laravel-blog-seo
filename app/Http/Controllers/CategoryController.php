d19086bc990b12d99811667b04edd62893df683c
Irwanto Wibowo <42676690+rinapratama335@users.noreply.github.com>
2020-05-08 05:11:05 +0700

    delete data kategori (#7)

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -103,6 +103,9 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $category = Category::findorfail($id);
+        $category->delete();
+
+        return redirect()->back()->with('success', 'Data berhasil dihapus');
     }
 }
