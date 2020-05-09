5331ca72737501b57b5ae65386569083555f698c
Irwanto Wibowo <rinapratama335@gmail.com>
2020-05-10 00:58:43 +0700

    membuat relasi many to many di tabel tags

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -35,6 +35,14 @@
             </select>
         </div>
         <div class="form-group">
+            <label>Pilih Tags</label>
+            <select class="form-control select2" multiple="" name="tags[]">
+                @foreach($tags as $tag)
+                    <option value="{{ $tag->id }}">{{ $tag->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="form-group">
             <label>Konten</label>
             <textarea class="form-control" name="content"></textarea>
         </div>
