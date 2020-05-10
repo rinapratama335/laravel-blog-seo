b0ea1db3f7d85840959b18f3139b345e6e9843bd
Irwanto Wibowo <42676690+rinapratama335@users.noreply.github.com>
2020-05-10 15:15:35 +0700

    membuat edit data post (#12)

---
+++ resources/views/admin/post/edit.blade.php
@@ -0,0 +1,68 @@
+@extends('template_backend.home')
+
+@section('subjudul', 'Tambah Post')
+
+@section('content')
+    @if(count($errors) > 0)
+        @foreach($errors->all() as $error)
+            <div class="alert alert-danger">
+                {{ $error }}
+            </div>
+        @endforeach
+    @endif
+
+    @if(Session::has('success'))
+        <div class="alert alert-success">
+            {{ Session('success') }}
+        </div>
+    @endif
+
+    <form method="post" action="{{ route('post.update', $post->id) }}" enctype="multipart/form-data">
+        @csrf {{-- Ini wajib ada di form laravel, karena untuk keamanan --}}
+        @method('patch')
+        <div class="form-group">
+            <label>Judul</label>
+            <input type="text" class="form-control" name="judul" value="{{ $post->judul }}">
+        </div>
+        <div class="form-group">
+            <label>Kategori</label>
+            <select name="category_id" id="" class="form-control">
+                <option value="" holder>Pilih kategori</option>
+                @foreach($category as $data)
+                    <option value="{{ $data->id }}"
+                        @if($post->category_id == $data->id)
+                            selected
+                        @endif
+                    >
+                        {{ $data->name }}
+                    </option>
+                @endforeach
+            </select>
+        </div>
+        <div class="form-group">
+            <label>Pilih Tags</label>
+            <select class="form-control select2" multiple="" name="tags[]">
+                @foreach($tags as $tag)
+                    <option value="{{ $tag->id }}"
+                        @foreach($post->tags as $value)
+                            @if($tag->id == $value->id)
+                                selected
+                            @endif
+                        @endforeach
+                    >{{ $tag->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="form-group">
+            <label>Konten</label>
+            <textarea class="form-control" name="content">{{ $post->content }}</textarea>
+        </div>
+        <div class="form-group">
+            <label>Gambar</label>
+            <input type="file" class="form-control" name="gambar">
+        </div>
+        <div class="form-group">
+            <button class="btn btn-primary btn-sm btn-block">Simpan</button>
+        </div>
+    </form>
+@endsection
